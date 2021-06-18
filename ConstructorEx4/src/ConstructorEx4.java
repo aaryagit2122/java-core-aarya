d67b71a01a0c29297636f75945947eddcdb5bda9
Aarya <Aarya@Aarya>
2021-06-18 11:54:06 +0530

    Constructor Ex4 Completed

---
+++ ConstructorEx4/src/ConstructorEx4.java
@@ -0,0 +1,14 @@
+
+public class ConstructorEx4 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Eclipse");
+		
+		
+		Employee e1 = new Employee(191, "Aarya", 80000);
+		
+		e1.showEmployee();
+
+	}
+
+}
